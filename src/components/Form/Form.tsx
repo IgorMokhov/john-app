a7014dfd6ae266b68c04e35f04ec4455461fc45f
Igor Mokhov <igormokhovid@gmail.com>
2024-03-21 09:22:38 +0300

    Added form validation

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -6,12 +6,45 @@ interface FormProps {
   handleClick: (login: string, password: string) => void;
 }
 
+interface Errors {
+  email: string | undefined;
+  password: string | undefined;
+}
+
 export const Form: FC<FormProps> = ({ title, handleClick }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState<Errors>({ email: '', password: '' });
+
+  const validateEmail = (email: string) => {
+    if (!email) {
+      return 'Email cannot be empty!';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      return 'Invalid email format!';
+    }
+
+    return '';
+  };
+
+  const validatePassword = (password: string) => {
+    if (!password) {
+      return 'Password cannot be empty!';
+    } else if (password.length < 6) {
+      return 'Password must be longer than 6 characters!';
+    }
+
+    return '';
+  };
 
   const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
+
+    const emailError = validateEmail(email);
+    const passwordError = validatePassword(password);
+
+    if (emailError || passwordError) {
+      setErrors({ email: emailError, password: passwordError });
+    }
 
     handleClick(email, password);
   };
@@ -29,6 +62,7 @@ export const Form: FC<FormProps> = ({ title, handleClick }) => {
             setEmail(e.target.value)
           }
         />
+        {errors.email && <p className={styles.error}>{errors.email}</p>}
       </label>
       <label htmlFor="pasword">
         Password
@@ -41,6 +75,7 @@ export const Form: FC<FormProps> = ({ title, handleClick }) => {
             setPassword(e.target.value)
           }
         />
+        {errors.password && <p className={styles.error}>{errors.password}</p>}
       </label>
       <button onClick={handleButtonClick} type="submit">
         {title}
